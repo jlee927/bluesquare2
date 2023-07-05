d47840c9dff9abbb10a3dadf7b454a623fc4d44b
jlee927 <137582674+jlee927@users.noreply.github.com>
2023-07-05 11:14:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function buttonColor()  {
+    document.getElementById("rectangle").style.backgroundColor='#00f';
+}
+
+function changeBack()   {
+    document.getElementById("rectangle").style.backgroundColor='#555';
+}
