c942df1660dacc2d4ae4e4c8e76dcf8e83860032
jlee927 <137582674+jlee927@users.noreply.github.com>
2023-07-10 12:36:26 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function buttonColor()  {
-    document.getElementById("rectangle").style.backgroundColor='#00f';
-}
-
-function changeBack()   {
-    document.getElementById("rectangle").style.backgroundColor='#555';
-}
